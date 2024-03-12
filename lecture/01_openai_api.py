f0f83c22913067741dbf6a82d1e61082af15f164
Woohee Kim <rladngml2002@naver.com>
2024-03-12 12:38:14 +0900

    Add openai chat

---
+++ lecture/01_openai_api.py
@@ -0,0 +1,31 @@
+#open API 사용하기
+# platform.openai.com
+# 1. API-KEY 발급
+# 2. 카드 등록(visa, master)+5.5달러(보유)
+
+
+# 라이브러리 관리
+# 1. venv 
+# 2. anaconda
+
+# 1. 챗봇 만들기
+# - ChatGPT : 챗봇 서비스 이름 (ex : 카카오톡) 
+#   > 인공지능 모델 : GPT
+#   > 무료 : 3.5 유로버전과 성능차이 심함
+#   > 유로 : 4.0
+
+# OpenAI 회사에서 GPT 관련 API 제공
+#  - https://openai.com/blog/openai-api
+
+from openai import OpenAI
+client = OpenAI(api_key="")
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": ""},
+    {"role": "user", "content": "클라우드 설명해줘."}
+  ]
+)
+
+print(completion.choices[0].message)
